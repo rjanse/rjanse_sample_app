b2a54cb4515db5ff4d56b7714539ce420c0f8aa6
Robin <robinjanse@gmail.com>
2010-04-27 17:55:40 +0200

    Done with sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20100421160024
+# Schema version: 20100426210416
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  remember_token     :string(255)
 #
 
 require 'spec_helper'
